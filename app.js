09fe9d83e9b4506afd06cc768c5873e3470883e2
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-03-14 17:20:26 +0200

    Adding some routing

--- app.js
+++ app.js
@@ -3,14 +3,36 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var nav = [{
+				Link: '/Books', 
+				Text: 'Book'
+			}, {
+				Link: '/Authors', 
+				Text: 'Author'
+			}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
 // app.use(express.static('src/views'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
+
 app.get('/', function(req, res) {
-	res.render('index', {title: 'Hello from render', list: ['a', 'b']});
+	res.render('index', {
+		title: 'Hello from render', 
+		nav: [{
+			Link: '/Books', 
+			Text: 'Books'
+		}, {
+			Link: '/Authors', 
+			Text: 'Authors'
+		}]
+	});
 });
 
 app.get('/books', function(req, res) {
