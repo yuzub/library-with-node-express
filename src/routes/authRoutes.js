f4dd2774ac6f925b15cc0bb2e360fcaaa716471f
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2017-03-16 01:52:23 +0200

    Working with passport js yomayo :(

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,13 @@ var router = function() {
 	authRouter.route('/signUp')
 		.post(function(req, res) {
 			console.log(req.body);
+			req.login(req.body, function() {
+				res.redirect('/auth/profile');
+			});
+		});
+	authRouter.route('/profile')
+		.get(function(req, res) {
+			res.json(req.user);
 		});
 	return authRouter;
 };
